9495dd8aee1ed83a50071faaaad21886fb9396b5
Your Name Here <lawsonezekiel68@gmail.com>
2026-05-20 10:50:08 +0100

    Fix the API refrence page broken link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -19,16 +19,17 @@ const config = {
     v4: true, // Improve compatibility with the upcoming Docusaurus v4
   },
 
-  // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
-
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+   // Set the production url of your site here
+   url: 'https://emilsone.github.io',
+   // Set the /<baseUrl>/ pathname under which your site is served
+   // For GitHub pages deployment, it is often '/<projectName>/'
+   baseUrl: '/my-docs-site/',
+ 
+   // GitHub pages deployment config.
+   // If you aren't using GitHub pages, you don't need these.
+   organizationName: 'emilsone', 
+   projectName: 'my-docs-site', 
+   deploymentBranch: 'gh-pages', 
 
   onBrokenLinks: 'throw',
 
@@ -95,7 +96,6 @@ const config = {
             position: 'left',
             label: 'Tutorial',
           },
-          
           {to: '/blog', label: 'Blog', position: 'left'},
           {
             href: 'https://github.com/facebook/docusaurus',
